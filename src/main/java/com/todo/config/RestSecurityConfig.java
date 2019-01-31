0d92034fa8f626df5b1ac2af15ad8636ff4fd87b
Jonathan Camargo <jcamargos@psl.com.co>
2019-01-31 17:39:03 -0500

    noop

--- src/main/java/com/todo/config/RestSecurityConfig.java
+++ src/main/java/com/todo/config/RestSecurityConfig.java
@@ -1,38 +1,30 @@
 package com.todo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private UserDetailsService userDetailsService;
+	private BasicAuthenticationEntryPointImpl authEntryPoint;
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("user1Pass"))
-				.authorities("ROLE_USER");
+		auth.inMemoryAuthentication().withUser("user1").password("{noop}user1Pass").roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 
-		http.authorizeRequests().anyRequest().permitAll();
+		http.cors().and().authorizeRequests().anyRequest().authenticated().and().httpBasic()
+				.authenticationEntryPoint(authEntryPoint);
 
 		// To modify the value of X-FRAME-OPTIONS to allow frames from SameOrigin for h2 DB Console
 		http.headers().frameOptions().sameOrigin();
-	}
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
